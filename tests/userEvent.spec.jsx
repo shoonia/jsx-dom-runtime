d3675f4cacc8cc79359d375cfdd7b08b2700cd22
shoonia <alexanderz@wix.com>
2021-03-06 14:49:15 +0200

    Add test for dblclick event

--- tests/userEvent.spec.jsx
+++ tests/userEvent.spec.jsx
@@ -1,12 +1,21 @@
 import userEvent from '@testing-library/user-event';
 
-describe('Click handler', () => {
+describe('User events', () => {
   it('should add click handler', () => {
     const spyCallback = jest.fn();
     const btn = <button type="button" onClick={spyCallback}>click</button>;
-
     // @ts-ignore
     userEvent.click(btn);
+
+    expect(spyCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should add double click handler', () => {
+    const spyCallback = jest.fn();
+    // @ts-ignore
+    const btn = <button type="button" onDBlClick={spyCallback}>click</button>;
+    // @ts-ignore
+    userEvent.dblClick(btn);
 
     expect(spyCallback).toHaveBeenCalledTimes(1);
   });
