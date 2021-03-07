4530533c0afcf3198c0a97175f2b92f3e90c90f6
shoonia <alexanderz@wix.com>
2021-03-07 16:42:12 +0200

    Setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,107 @@
 import { join } from 'path';
-import pkg from './jsx-runtime/package.json';
+import { emptyDirSync, outputJSONSync } from 'fs-extra';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
-const resolve = (path) => join(pkg.name, path);
+const dist = 'jsx-runtime';
+const amdName = 'jsxRuntime';
+const source = 'jsxRuntime.js';
+const esm = 'jsxRuntime.esm.js';
+const moduleJs = 'jsxRuntime.module.js';
+const cjs = 'jsxRuntime.cjs.js';
+const umd = 'jsxRuntime.umd.js';
+const amd = 'jsxRuntime.amd.js';
+
+const pkg = {
+  name: dist,
+  amdName,
+  version: '1.0.0',
+  description: 'JSX runtime',
+  source,
+  main: cjs,
+  module: moduleJs,
+  exports: `./${esm}`,
+  esmodule: esm,
+  unpkg: umd,
+  'umd:main': umd,
+  private: true,
+  license: 'MIT',
+};
+
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      loose: true,
+      targets: 'defaults',
+    },
+  ],
+];
+
+const resolve = (path) => join(dist, path);
+
+emptyDirSync(dist);
+outputJSONSync(resolve('package.json'), pkg, { spaces: 2 });
 
 export default {
-  input: resolve(pkg.source),
+  input: './src/index.js',
   output: [
     {
-      file: resolve(pkg.main),
-      format: 'cjs',
-    },
-    {
-      file: resolve(pkg.module),
+      file: resolve(source),
       format: 'esm',
     },
     {
-      file: resolve(pkg['umd:main']),
-      format: 'umd',
-      name: pkg.amdName,
+      file: resolve(esm),
+      format: 'esm',
+      plugins: [
+        terser(),
+      ],
+    },
+    {
+      file: resolve(moduleJs),
+      format: 'esm',
+      plugins: [
+        getBabelOutputPlugin({
+          presets
+        }),
+        terser(),
+      ],
+    },
+    {
+      file: resolve(cjs),
+      format: 'cjs',
+      plugins: [
+        getBabelOutputPlugin({
+          presets
+        }),
+        terser(),
+      ],
+    },
+    {
+      name: amdName,
+      file: resolve(amd),
+      plugins: [
+        getBabelOutputPlugin({
+          presets,
+          plugins: [
+            '@babel/plugin-transform-modules-amd',
+          ],
+        }),
+        terser(),
+      ],
+    },
+    {
+      name: amdName,
+      file: resolve(umd),
+      plugins: [
+        getBabelOutputPlugin({
+          presets,
+          plugins: [
+            '@babel/plugin-transform-modules-umd',
+          ],
+        }),
+        terser(),
+      ],
     },
   ],
 };
