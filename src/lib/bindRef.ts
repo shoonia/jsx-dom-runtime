f99340fd2cfcb74a308077fe85445ea14590d3b1
shoonia <alexanderz@wix.com>
2021-03-14 16:39:24 +0200

    Change to increment loop

--- src/lib/bindRef.ts
+++ src/lib/bindRef.ts
@@ -1,10 +1,9 @@
 export function bindRef() {
   let refs = arguments;
-  let i = refs.length;
 
   return (node: HTMLElement) => {
-    while (0 < i--) {
-      let ref = refs[i];
+    for (let i = 0; i < refs.length;) {
+      let ref = refs[i++];
 
       if (typeof ref === 'function') {
         ref(node);
