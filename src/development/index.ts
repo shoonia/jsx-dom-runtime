4c2247b3e09673d04a983096b3997230c0f03269
shoonia <alexanderz@wix.com>
2021-03-16 00:03:33 +0200

    Fix loop

--- src/development/index.ts
+++ src/development/index.ts
@@ -2,14 +2,16 @@ import { jsx } from '../jsxRuntime';
 import { Fragment } from '../Fragment';
 import { specMap } from './specMap';
 
+type Props = Record<string, unknown>;
+
 interface IStack {
   fileName: string;
   lineNumber: number;
   columnNumber: number;
 }
 
-const propTypes = (tagName: string, spec, props, stack: IStack) => {
-  for (const key in spec) {
+const propTypes = (tagName: string, spec, props: Props, stack: IStack) => {
+  for (const key in props) {
     let error;
 
     try {
