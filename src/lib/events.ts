9e6aa64d2a1257ca72200d7f7a553184a1c12f23
shoonia <alexanderz@wix.com>
2021-03-21 18:44:28 +0200

    Reduce size

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,13 +1,7 @@
-export let events = (ready) => {
-  return (node: HTMLElement) => {
-    let on = (type, listener, options) => {
-      node.addEventListener(type, listener, options);
-    };
-
-    let off = (type, listener, options) => {
-      node.removeEventListener(type, listener, options);
-    };
-
-    ready(on, off, node);
-  };
+export let events = (ready) => (node: HTMLElement) => {
+  ready((type, listener, options) => {
+    node.addEventListener(type, listener, options);
+  }, (type, listener, options) => {
+    node.removeEventListener(type, listener, options);
+  }, node);
 };
