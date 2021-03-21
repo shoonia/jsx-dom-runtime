85c4a432e6b490a9fc751dafed235b8df289756f
shoonia <alexanderz@wix.com>
2021-03-22 00:18:56 +0200

    Add tests

--- tests/table-element.spec.tsx
+++ tests/table-element.spec.tsx
@@ -2,6 +2,7 @@ describe('HTMLTableElement', () => {
   it('should render a table', () => {
     expect(
       <table>
+        <caption>Title</caption>
         <thead>
           <tr>
             <th colspan="2">The table header</th>
@@ -15,7 +16,7 @@ describe('HTMLTableElement', () => {
         </tbody>
       </table>
     ).toHaveOuterHTML(
-      '<table><thead><tr><th colspan="2">The table header</th></tr></thead><tbody><tr><td>The table body</td><td>with two columns</td></tr></tbody></table>'
+      '<table><caption>Title</caption><thead><tr><th colspan="2">The table header</th></tr></thead><tbody><tr><td>The table body</td><td>with two columns</td></tr></tbody></table>'
     );
   });
 });
