773a13a4faf6b7e9d22f46bd3b385318c9549c55
shoonia <alexanderz@wix.com>
2021-03-30 22:21:43 +0300

    Refactoring

--- src/jsxRuntime.js
+++ src/jsxRuntime.js
@@ -21,69 +21,67 @@ export let jsx = (node, props) => {
     for (key in props) {
       extensions.set(key, props[key]);
     }
+  } else {
+    node = typeof node === 'string' ? document.createElement(node) : node;
 
-    return;
-  }
+    for (key in props) {
+      if (key !== 'ref' && key !== 'children') {
+        val = props[key];
 
-  node = typeof node === 'string' ? document.createElement(node) : node;
-
-  for (key in props) {
-    if (key !== 'ref' && key !== 'children') {
-      val = props[key];
-
-      if (extensions.has(key)) {
-        extensions.get(key)(node, val);
-      } else if (properties.has(key)) {
-        node[key] = val;
-      } else if (key === 'style') {
-        if (typeof val === 'string') {
-          node.style.cssText = val;
-        } else {
+        if (extensions.has(key)) {
+          extensions.get(key)(node, val);
+        } else if (properties.has(key)) {
+          node[key] = val;
+        } else if (key === 'style') {
+          if (typeof val === 'string') {
+            node.style.cssText = val;
+          } else {
           // reuse `key` variable
-          for (key in val) {
-            if (key[0] === '-') {
-              node.style.setProperty(key, val[key]);
-            } else {
-              node.style[key] = val[key];
+            for (key in val) {
+              if (key[0] === '-') {
+                node.style.setProperty(key, val[key]);
+              } else {
+                node.style[key] = val[key];
+              }
             }
           }
-        }
         // Benchmark for comparison (thanks preact): https://esbench.com/bench/574c954bdb965b9a00965ac6
-      } else if (key[0] === 'o' && key[1] === 'n') {
-        key = key.toLowerCase();
+        } else if (key[0] === 'o' && key[1] === 'n') {
+          key = key.toLowerCase();
 
-        if (key in node) {
-          node[key] = val;
-        }
-      } else if (val != null) {
-        if (typeof val !== 'boolean' || /^(ari|dat)a-/.test(key)) {
-          node.setAttribute(key, val);
-        } else if (val) {
-          node.setAttribute(key, '');
+          if (key in node) {
+            node[key] = val;
+          }
+        } else if (val != null) {
+          if (typeof val !== 'boolean' || /^(ari|dat)a-/.test(key)) {
+            node.setAttribute(key, val);
+          } else if (val) {
+            node.setAttribute(key, '');
+          } else {
+            node.removeAttribute(key);
+          }
         } else {
           node.removeAttribute(key);
         }
-      } else {
-        node.removeAttribute(key);
       }
     }
-  }
 
-  appendChildren(
-    node.tagName === 'TEMPLATE' ? node.content : node,
-    props.children
-  );
+    appendChildren(
+      node.tagName === 'TEMPLATE' ? node.content : node,
+      props.children
+    );
 
-  // reuse `val` variable
-  val = props.ref;
+    // reuse `val` variable
+    val = props.ref;
 
-  if (val != null) {
-    if (typeof val === 'function') {
-      val(node);
-    } else {
-      val.current = node;
+    if (val != null) {
+      if (typeof val === 'function') {
+        val(node);
+      } else {
+        val.current = node;
+      }
     }
-  }
 
-  return node;
+    return node;
+  }
 };
