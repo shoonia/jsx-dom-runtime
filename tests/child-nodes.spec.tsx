e3611b1c6c0fbc6d659f1b0951759d4bb71df5a0
shoonia <alexanderz@wix.com>
2021-10-10 12:34:40 +0300

    Remove smoke tests

--- tests/child-nodes.spec.tsx
+++ tests/child-nodes.spec.tsx
@@ -1,4 +1,83 @@
 describe('Child nodes', () => {
+  it('should have a text', () => {
+    expect(<span>hello</span>).toHaveOuterHTML('<span>hello</span>');
+  });
+
+  it.each([
+    [0, /******************/ '<p>0</p>'],
+    [1, /******************/ '<p>1</p>'],
+    [5.5, /****************/ '<p>5.5</p>'],
+    [-1, /*****************/ '<p>-1</p>'],
+    [126, /****************/ '<p>126</p>'],
+    [NaN, /****************/ '<p>NaN</p>'],
+    [10n, /****************/ '<p>10</p>'],
+    [Infinity, /***********/ '<p>Infinity</p>'],
+    [-Infinity, /**********/ '<p>-Infinity</p>'],
+    [true, /***************/ '<p>true</p>'],
+    [[0, 1, NaN, true], /**/ '<p>01NaNtrue</p>'],
+    [{}, /*****************/ '<p>[object Object]</p>'],
+  ])('should have a stringified child node if %s', (val, html) => {
+    // @ts-expect-error
+    expect(<p>{val}</p>).toHaveOuterHTML(html);
+  });
+
+  it.each([
+    '',
+    undefined,
+    null,
+    false,
+    [],
+    [[]],
+    [[], undefined, null, false, ''],
+  ])('should have empty tag if %s', (child) => {
+    expect(<div>{child}</div>).toHaveOuterHTML('<div></div>');
+  });
+
+  it('should have two children nodes', () => {
+    expect(
+      <ul>
+        <li>one</li>
+        <li>two</li>
+      </ul>
+    ).toHaveOuterHTML(
+      '<ul><li>one</li><li>two</li></ul>',
+    );
+  });
+
+  it('should have two children nodes with text', () => {
+    expect(
+      <pre>
+        one
+        <code>two</code>
+      </pre>
+    ).toHaveOuterHTML(
+      '<pre>one<code>two</code></pre>',
+    );
+  });
+
+  it('should have two children nodes as array', () => {
+    expect(
+      <div>
+        {
+          [
+            <nav />,
+            <span />,
+            'text',
+          ]
+        }
+      </div>
+    ).toHaveOuterHTML(
+      '<div><nav></nav><span></span>text</div>',
+    );
+  });
+
+  it('should add custom Web Component tag', () => {
+    // @ts-expect-error
+    expect(<my-webcomponent></my-webcomponent>).toHaveOuterHTML(
+      '<my-webcomponent></my-webcomponent>',
+    );
+  });
+
   it('should work with deep nested child nodes', () => {
     expect(
       <div>
