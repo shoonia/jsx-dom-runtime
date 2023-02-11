f8daa2005a538a5efb249bfe27df98eaac0e8fab
shoonia <alexanderz@wix.com>
2023-02-11 16:59:32 +0200

    Add test

--- tests/userEvent.spec.tsx
+++ tests/userEvent.spec.tsx
@@ -3,39 +3,52 @@ import { jest } from '@jest/globals';
 
 describe('User events', () => {
   it('should add click handler', () => {
-    const spyCallback = jest.fn();
-    const btn = <button type="button" onClick={spyCallback}>click</button>;
+    const spy = jest.fn();
+    const btn = <button type="button" onClick={spy}>click</button>;
 
     fireEvent.click(btn);
 
-    expect(spyCallback).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should add double click handler', () => {
-    const spyCallback = jest.fn();
-    const btn = <button type="button" onDBlClick={spyCallback}>click</button>;
+    const spy = jest.fn();
+    const btn = <button type="button" onDBlClick={spy}>click</button>;
 
     fireEvent.dblClick(btn);
 
-    expect(spyCallback).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should add change handler', () => {
-    const spyCallback = jest.fn();
-    const input = <input type="text" onChange={spyCallback} />;
+    const spy = jest.fn();
+    const input = <input type="text" onChange={spy} />;
 
     fireEvent.change(input, { target: { value: 'change event' } });
 
-    expect(spyCallback).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledTimes(1);
     expect(input).toHaveValue('change event');
   });
 
   it('should add submit handler', () => {
-    const spyCallback = jest.fn();
-    const form = <form onSubmit={spyCallback} />;
+    const spy = jest.fn();
+    const form = <form onSubmit={spy} />;
 
     fireEvent.submit(form);
 
-    expect(spyCallback).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should add lower case name handlers', () => {
+    const spyClick = jest.fn();
+    const spyChange = jest.fn();
+    const input = <input type="text" onclick={spyClick} onchange={spyChange} />;
+
+    fireEvent.click(input);
+    fireEvent.change(input, { target: { value: 'xyz' } });
+
+    expect(spyClick).toHaveBeenCalledTimes(1);
+    expect(spyChange).toHaveBeenCalledTimes(1);
+    expect(input).toHaveValue('xyz');
   });
 });
