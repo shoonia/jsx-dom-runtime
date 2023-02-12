b914ecda0c22aab40a65cd74d10bffe66771b000
shoonia <alexanderz@wix.com>
2023-02-12 16:42:05 +0200

    Add tests

--- tests/className.spec.tsx
+++ tests/className.spec.tsx
@@ -6,4 +6,15 @@ describe('className / class', () => {
   it('should have a class attribute', () => {
     expect(<i class="a b c" />).toHaveClass('a b c');
   });
+
+  it('should have not break function componet props', () => {
+    const App = ({ className }) => <div data-test={className} />;
+
+    expect(<App className="qa" />).toHaveOuterHTML('<div data-test="qa"></div>');
+  });
+
+  it('should have not break Web Component attr', () => {
+    // @ts-expect-error
+    expect(<qa-test classname="name"></qa-test>).toHaveOuterHTML('<qa-test classname="name"></qa-test>');
+  });
 });
