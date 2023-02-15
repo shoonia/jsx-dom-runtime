20839c727bd473b5430cea9d4fdee954dd7551bb
shoonia <alexanderz@wix.com>
2023-02-15 13:13:35 +0200

    Update SVG support

--- src/babel/tags/svg.js
+++ src/babel/tags/svg.js
@@ -1,18 +1,19 @@
 const svg = new Set([
-  'svg', 'animate', 'circle', 'animateTransform', 'clipPath', 'defs',
-  'desc', 'ellipse', 'feBlend', 'feColorMatrix', 'feComponentTransfer',
+  'animate', 'animateMotion', 'animateTransform', 'circle', 'clipPath',
+  'defs', 'desc', 'ellipse', 'feBlend', 'feColorMatrix', 'feComponentTransfer',
   'feComposite', 'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap',
-  'feDropShadow', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR',
-  'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology',
-  'feOffset', 'feSpecularLighting', 'feTile', 'feTurbulence', 'filter',
-  'foreignObject', 'g', 'image', 'line', 'linearGradient', 'marker', 'mask',
-  'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect',
-  'stop', 'symbol', 'text', 'textPath', 'tspan', 'use',
+  'feDistantLight', 'feDropShadow', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG',
+  'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology',
+  'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile',
+  'feTurbulence', 'filter', 'foreignObject', 'g', 'image', 'line', 'linearGradient',
+  'marker', 'mask', 'metadata', 'mpath', 'path', 'pattern', 'polygon', 'polyline',
+  'radialGradient', 'rect', 'set', 'stop', 'svg', 'switch', 'symbol', 'text', 'textPath',
+  'tspan', 'use', 'view',
 ]);
 
-const maybeSvg = new Set(['a', 'link', 'script', 'style', 'title']);
+const maybeSvg = new Set(['a', 'script', 'style', 'title']);
 
 export const isSvgElement = (path) => {
   return svg.has(path.node.name.name) ||
-  (maybeSvg.has(path.node.name.name) && svg.has(path.parentPath.parent?.openingElement?.name?.name));
+    (maybeSvg.has(path.node.name.name) && svg.has(path.parentPath.parent?.openingElement?.name?.name));
 };
