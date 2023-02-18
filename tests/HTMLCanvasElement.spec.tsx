c5c702ae737fe4c9830ddecff7baebd5226bf496
shoonia <alexanderz@wix.com>
2023-02-19 00:21:09 +0200

    Add tests

---
+++ tests/HTMLCanvasElement.spec.tsx
@@ -0,0 +1,27 @@
+describe('HTMLCanvasElement', () => {
+  it('should render <canvas> tag', () => {
+    expect(
+      <canvas width="300" height="300">
+        An alternative text describing what your canvas displays.
+      </canvas>
+    ).toHaveOuterHTML(
+      '<canvas width="300" height="300">An alternative text describing what your canvas displays.</canvas>'
+    );
+  });
+
+  it('should have `lang` attribute & property', () => {
+    expect(<canvas width="100" />).toHaveAttribute('width', '100');
+    expect(<canvas width="100" />).toHaveProperty('width', 100);
+
+    expect(<canvas width={100} />).toHaveAttribute('width', '100');
+    expect(<canvas width={100} />).toHaveProperty('width', 100);
+  });
+
+  it('should have `lang` attribute & property', () => {
+    expect(<canvas height="200" />).toHaveAttribute('height', '200');
+    expect(<canvas height="200" />).toHaveProperty('height', 200);
+
+    expect(<canvas height={200} />).toHaveAttribute('height', '200');
+    expect(<canvas height={200} />).toHaveProperty('height', 200);
+  });
+});
