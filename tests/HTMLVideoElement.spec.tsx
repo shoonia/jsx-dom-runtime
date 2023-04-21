21a996ff8ba6982ca3f7ddf0a3279427062cadff
shoonia <alexanderz@wix.com>
2023-04-21 21:14:17 +0300

    Add tests

--- tests/HTMLVideoElement.spec.tsx
+++ tests/HTMLVideoElement.spec.tsx
@@ -3,4 +3,30 @@ describe('HTMLVideoElement', () => {
     expect(<video muted />).toHaveProperty('muted', true);
     expect(<video muted={false} />).toHaveProperty('muted', false);
   });
+
+  it('should NOT have `muted` attribute', () => {
+    expect(<video muted />).not.toHaveAttribute('muted');
+    expect(<video muted={false} />).not.toHaveAttribute('muted');
+  });
+
+  it('should have `src` attribute & property', () => {
+    expect(<video src="/video" />).toHaveProperty('src', 'http://localhost/video');
+    expect(<video src="/video" />).toHaveAttribute('src', '/video');
+  });
+
+  it('should have `poster` attribute & property', () => {
+    expect(<video poster="/poster" />).toHaveProperty('poster', 'http://localhost/poster');
+    expect(<video poster="/poster" />).toHaveAttribute('poster', '/poster');
+  });
+
+  it('should have `playsInline` attribure & property', () => {
+    expect(<video playsInline />).toHaveProperty('playsInline', true);
+    expect(<video playsInline={false} />).toHaveProperty('playsInline', false);
+    expect(<video playsInline />).toHaveAttribute('playsinline', '');
+  });
+
+  it('should NOT have `playsInline` attribute', () => {
+    expect(<video />).not.toHaveAttribute('playsinline');
+    expect(<video playsInline={false} />).not.toHaveAttribute('playsinline');
+  });
 });
