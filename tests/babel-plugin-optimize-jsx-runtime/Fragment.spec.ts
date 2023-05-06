173d10702f236cc829e4592c1e714ec18f9d3f0b
shoonia <alexanderz@wix.com>
2023-05-07 00:54:39 +0300

    Add tests

--- tests/babel-plugin-optimize-jsx-runtime/Fragment.spec.ts
+++ tests/babel-plugin-optimize-jsx-runtime/Fragment.spec.ts
@@ -15,4 +15,24 @@ describe('babel-plugin-optimize-jsx-runtime: Frament', () => {
     const result = await t('<><App key="value" /></>;');
     expect(result).toBe('<>{App({key:"value"})}</>;');
   });
+
+  it('should inline with attibute number literal', async () => {
+    const result = await t('<><App index={1} /></>;');
+    expect(result).toBe('<>{App({index:1})}</>;');
+  });
+
+  it('should inline with attibute string literal', async () => {
+    const result = await t('<><App index={"1"} /></>;');
+    expect(result).toBe('<>{App({index:"1"})}</>;');
+  });
+
+  it('should inline with attibute boolean (true) literal', async () => {
+    const result = await t('<><App index /></>;');
+    expect(result).toBe('<>{App({index:true})}</>;');
+  });
+
+  it('should inline with attibute boolean (false) literal', async () => {
+    const result = await t('<><App index={false} /></>;');
+    expect(result).toBe('<>{App({index:false})}</>;');
+  });
 });
--- tests/babel-plugin-optimize-jsx-runtime/element.spec.ts
+++ tests/babel-plugin-optimize-jsx-runtime/element.spec.ts
@@ -15,4 +15,24 @@ describe('babel-plugin-optimize-jsx-runtime: Element', () => {
     const result = await t('<span><App key="value" /></span>;');
     expect(result).toBe('<span>{App({key:"value"})}</span>;');
   });
+
+  it('should inline with attibute number literal', async () => {
+    const result = await t('<div><App index={1} /></div>;');
+    expect(result).toBe('<div>{App({index:1})}</div>;');
+  });
+
+  it('should inline with attibute string literal', async () => {
+    const result = await t('<div><App index={"1"} /></div>;');
+    expect(result).toBe('<div>{App({index:"1"})}</div>;');
+  });
+
+  it('should inline with attibute boolean (true) literal', async () => {
+    const result = await t('<div><App index /></div>;');
+    expect(result).toBe('<div>{App({index:true})}</div>;');
+  });
+
+  it('should inline with attibute boolean (false) literal', async () => {
+    const result = await t('<div><App index={false} /></div>;');
+    expect(result).toBe('<div>{App({index:false})}</div>;');
+  });
 });
