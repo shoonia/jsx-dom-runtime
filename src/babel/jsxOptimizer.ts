4f383e6ce4d6e11ede319906dd14b00010758691
shoonia <alexanderz@wix.com>
2023-05-08 19:50:21 +0300

    Refactoring

--- src/babel/jsxOptimizer.ts
+++ src/babel/jsxOptimizer.ts
@@ -1,5 +1,55 @@
-import type { PluginObj } from '@babel/core';
+import type { NodePath, PluginObj } from '@babel/core';
 import t from '@babel/types';
+
+const createCallExpression = (
+  name: string,
+  path: NodePath<t.JSXOpeningElement>,
+): t.CallExpression => {
+  const props = path.node.attributes.map((attr) => {
+    if (t.isJSXSpreadAttribute(attr)) {
+      return t.spreadElement(attr.argument);
+    }
+
+    return t.objectProperty(
+      t.identifier(
+        t.isJSXNamespacedName(attr.name)
+          ? JSON.stringify(attr.name.namespace.name + ':' + attr.name.name.name)
+          : t.isValidIdentifier(attr.name.name)
+            ? attr.name.name
+            : JSON.stringify(attr.name.name),
+      ),
+      t.isJSXExpressionContainer(attr.value)
+        ? t.isJSXEmptyExpression(attr.value.expression)
+          ? t.nullLiteral()
+          : attr.value.expression
+        : attr.value || t.booleanLiteral(true),
+    );
+  });
+
+  if (t.isJSXElement(path.container)) {
+    const children = t.react.buildChildren(path.container).map((child) => {
+      return t.isJSXSpreadChild(child)
+        ? child.expression
+        : child;
+    });
+
+    if (children.length > 0) {
+      props.push(
+        t.objectProperty(
+          t.identifier('children'),
+          children.length === 1
+            ? children[0]
+            : t.arrayExpression(children),
+        ),
+      );
+    }
+  }
+
+  return t.callExpression(
+    t.identifier(name),
+    [t.objectExpression(props)],
+  );
+};
 
 export const jsxOptimizer = (): PluginObj => {
   return {
@@ -23,51 +73,8 @@ export const jsxOptimizer = (): PluginObj => {
             });
 
             if (index > -1) {
-              const props = path.node.attributes.map((attr) => {
-                if (t.isJSXSpreadAttribute(attr)) {
-                  return t.spreadElement(attr.argument);
-                }
-
-                return t.objectProperty(
-                  t.identifier(
-                    t.isJSXNamespacedName(attr.name)
-                      ? JSON.stringify(attr.name.namespace.name + ':' + attr.name.name.name)
-                      : t.isValidIdentifier(attr.name.name)
-                        ? attr.name.name
-                        : JSON.stringify(attr.name.name),
-                  ),
-                  t.isJSXExpressionContainer(attr.value)
-                    ? t.isJSXEmptyExpression(attr.value.expression)
-                      ? t.nullLiteral()
-                      : attr.value.expression
-                    : attr.value || t.booleanLiteral(true),
-                );
-              });
-
-              if (t.isJSXElement(path.container)) {
-                const children = t.react.buildChildren(path.container).map((child) => {
-                  return t.isJSXSpreadChild(child)
-                    ? child.expression
-                    : child;
-                });
-
-                if (children.length > 0) {
-                  props.push(
-                    t.objectProperty(
-                      t.identifier('children'),
-                      children.length === 1
-                        ? children[0]
-                        : t.arrayExpression(children),
-                    ),
-                  );
-                }
-              }
-
               parent.children[index] = t.jsxExpressionContainer(
-                t.callExpression(
-                  t.identifier(element.name),
-                  [t.objectExpression(props)],
-                ),
+                createCallExpression(element.name, path),
               );
             }
           }
