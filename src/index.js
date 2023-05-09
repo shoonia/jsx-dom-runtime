59ebb5482ea9cb9e2ae290f280209c8fae249639
shoonia <alexanderz@wix.com>
2023-05-09 23:57:52 +0300

    Remove jsxs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { Fragment } from './Fragment';
-export { jsx, jsx as jsxs, properties } from './jsxRuntime';
+export { jsx, properties } from './jsxRuntime';
 export { appendChildren } from './appendChildren';
 export { Extend } from './Extend';
 export { useRef } from './lib/useRef.ts';
