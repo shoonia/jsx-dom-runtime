693b242dd69c9addcc0e3731b4c841badebb1187
shoonia <alexanderz@wix.com>
2023-05-09 15:06:24 +0300

    Add ForStatement

---
+++ tests/babel-plugin-optimize-jsx-runtime/ForStatement.spec.ts
@@ -0,0 +1,23 @@
+import { t } from './transform';
+
+describe('babel-plugin-optimize-jsx-runtime: ForStatement', () => {
+  it('should work with `for` loop, init', async () => {
+    const result = await t('for (<App />;;) {};');
+    expect(result).toBe('for(App({});;){};');
+  });
+
+  it('should work with `for` loop, test', async () => {
+    const result = await t('for (;<App />;) {};');
+    expect(result).toBe('for(;App({});){};');
+  });
+
+  it('should work with `for` loop, update', async () => {
+    const result = await t('for (;;<App />) {};');
+    expect(result).toBe('for(;;App({})){};');
+  });
+
+  it('should work with `for` loop, all', async () => {
+    const result = await t('for (<Init />; <Test />; <Update />) {};');
+    expect(result).toBe('for(Init({});Test({});Update({})){};');
+  });
+});
