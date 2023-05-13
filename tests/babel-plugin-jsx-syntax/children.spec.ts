c1b58b78cf2db065561f6fe327fcf3bcce84848a
shoonia <alexanderz@wix.com>
2023-05-13 11:40:33 +0300

    Clear test

--- tests/babel-plugin-jsx-syntax/children.spec.ts
+++ tests/babel-plugin-jsx-syntax/children.spec.ts
@@ -1,45 +1,43 @@
 import { t } from './transform';
 
-const start = 'import{Fragment as _Fragment}from"jsx-dom-runtime";/*#__PURE__*/';
-
 describe('babel-plugin-jsx-syntax: Element', () => {
   it('should NOT have children', async () => {
-    const result = await t('<><App></App></>;');
-    expect(result).toBe(start + '_Fragment(App({}));');
+    const result = await t('<App></App>;');
+    expect(result).toBe('App({});');
   });
 
   it('should have one child number', async () => {
-    const result = await t('<><App>{1}</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:1}));');
+    const result = await t('<App>{1}</App>');
+    expect(result).toBe('App({children:1});');
   });
 
   it('should have one child string', async () => {
-    const result = await t('<><App>1</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:"1"}));');
+    const result = await t('<App>1</App>');
+    expect(result).toBe('App({children:"1"});');
   });
 
   it('should have three children', async () => {
-    const result = await t('<><App>{1} {2}</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:[1," ",2]}));');
+    const result = await t('<App>{1} {2}</App>');
+    expect(result).toBe('App({children:[1," ",2]});');
   });
 
   it('should return expression of JSXSpreadChild', async () => {
-    const result = await t('<><App>{...items}</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:items}));');
+    const result = await t('<App>{...items}</App>');
+    expect(result).toBe('App({children:items});');
   });
 
   it('should return expression of JSXSpreadChild on the start', async () => {
-    const result = await t('<><App>{...items} end</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:[items," end"]}));');
+    const result = await t('<App>{...items} end</App>');
+    expect(result).toBe('App({children:[items," end"]});');
   });
 
   it('should return expression of JSXSpreadChild on the end', async () => {
-    const result = await t('<><App>start {...items}</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:["start ",items]}));');
+    const result = await t('<App>start {...items}</App>');
+    expect(result).toBe('App({children:["start ",items]});');
   });
 
   it('should return expression of JSXSpreadChild between children', async () => {
-    const result = await t('<><App>start {...items} end</App></>');
-    expect(result).toBe(start + '_Fragment(App({children:["start ",items," end"]}));');
+    const result = await t('<App>start {...items} end</App>');
+    expect(result).toBe('App({children:["start ",items," end"]});');
   });
 });
