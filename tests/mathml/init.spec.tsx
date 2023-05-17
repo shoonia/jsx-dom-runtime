710a1ae8c4b5fe1c5c3c77e38d937cec84a6a231
shoonia <alexanderz@wix.com>
2023-05-17 09:17:36 +0300

    Add test

--- tests/mathml/init.spec.tsx
+++ tests/mathml/init.spec.tsx
@@ -1,9 +1,9 @@
 import { t } from '../babel-plugin-jsx-syntax/transform';
 
-const start = 'import{mathmlNS as _mathmlNS}from"jsx-dom-runtime";import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/';
+const start = 'import{mathmlNS as _mathmlNS}from"jsx-dom-runtime";import{jsx as _jsx}from"jsx-dom-runtime";';
 
 describe('MathML', () => {
-  it('should render MathML tags', async () => {
+  it('should transform MathML tags', async () => {
     const result = await t(
       `<math display="inline">
         <mfrac>
@@ -17,7 +17,25 @@ describe('MathML', () => {
     );
 
     expect(result).toBe(
-      start + '_jsx("math",{display:"inline",children:/*#__PURE__*/_jsx("mfrac",{children:[/*#__PURE__*/_jsx("msup",{children:[/*#__PURE__*/_jsx("mi",{children:"\\u03C0",__ns:_mathmlNS}),/*#__PURE__*/_jsx("mn",{children:"2",__ns:_mathmlNS})],__ns:_mathmlNS}),/*#__PURE__*/_jsx("mn",{children:"6",__ns:_mathmlNS})],__ns:_mathmlNS}),__ns:_mathmlNS});'
+      start + '/*#__PURE__*/_jsx("math",{display:"inline",children:/*#__PURE__*/_jsx("mfrac",{children:[/*#__PURE__*/_jsx("msup",{children:[/*#__PURE__*/_jsx("mi",{children:"\\u03C0",__ns:_mathmlNS}),/*#__PURE__*/_jsx("mn",{children:"2",__ns:_mathmlNS})],__ns:_mathmlNS}),/*#__PURE__*/_jsx("mn",{children:"6",__ns:_mathmlNS})],__ns:_mathmlNS}),__ns:_mathmlNS});'
+    );
+  });
+
+  it('should render MathML tags', async () => {
+    document.body.append(
+      <math display="inline">
+        <mfrac>
+          <msup>
+            <mi>π</mi>
+            <mn>2</mn>
+          </msup>
+          <mn>6</mn>
+        </mfrac>
+      </math>
+    );
+
+    expect(document.body).toHaveInnerHTML(
+      '<math display="inline"><mfrac><msup><mi>π</mi><mn>2</mn></msup><mn>6</mn></mfrac></math>'
     );
   });
 });
