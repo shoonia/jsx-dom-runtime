c2dd05d276337957f00f1242ac6ae32f757fd6bc
shoonia <alexanderz@wix.com>
2023-05-28 18:56:38 +0300

    Rename const

--- src/extensions/xlink.ts
+++ src/extensions/xlink.ts
@@ -1,10 +1,10 @@
 // @ts-ignore
 import { extensions } from '../jsx-runtime';
 
-export const xlinkNS = 'http://www.w3.org/1999/xlink';
+export const ns = 'http://www.w3.org/1999/xlink';
 
 const handler = (node: SVGElement, value: string, key: string): void =>
-  node.setAttributeNS(xlinkNS, key, value);
+  node.setAttributeNS(ns, key, value);
 
 export const xLink = (): void => [
   'actuate',
