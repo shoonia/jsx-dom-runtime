c11452b0cad3752c49167ff990b10a4ca68afb14
shoonia <alexanderz@wix.com>
2023-06-06 23:06:51 +0300

    Impl: import declaration

--- src/babel/createImport.ts
+++ src/babel/createImport.ts
@@ -1,26 +1,45 @@
 import type { NodePath } from '@babel/core';
-import t from '@babel/types';
-import { addNamed } from '@babel/helper-module-imports';
+import type t from '@babel/types';
 
-import { $identifier } from './builders';
+import { $identifier, $stringLiteral } from './builders';
 
 export type TImportName = 'jsx' | 'Fragment' | 'svgNS' | 'mathmlNS' | 'xhtmlNS';
 
 export const createImport = (path: NodePath<t.Program>) => {
+  const newImport = (): t.ImportDeclaration => {
+    const node: t.ImportDeclaration = {
+      type: 'ImportDeclaration',
+      specifiers: [],
+      source: $stringLiteral('jsx-dom-runtime'),
+      importKind: 'value',
+    };
+
+    path.unshiftContainer('body', node);
+
+    return node;
+  };
+
   const cache = new Map<string, string>();
+  let declaration: t.ImportDeclaration;
 
   return (importName: TImportName): t.Identifier => {
+    declaration ??= newImport();
+
     if (cache.has(importName)) {
       return $identifier(cache.get(importName));
     }
 
-    const newImport = addNamed(path, importName, 'jsx-dom-runtime', {
-      importedInterop: 'uncompiled',
-      importPosition: 'after',
-    });
+    const local = $identifier('_' + importName);
+    const specifier: t.ImportSpecifier = {
+      type: 'ImportSpecifier',
+      local,
+      imported: $identifier(importName),
+      importKind: 'value',
+    };
 
-    cache.set(importName, newImport.name);
+    declaration.specifiers.push(specifier);
+    cache.set(importName, local.name);
 
-    return newImport;
+    return local;
   };
 };
