19c5b3490879cda4611ea54eced65c34a8e0389d
shoonia <alexanderz@wix.com>
2023-06-06 23:25:21 +0300

    Add test

--- tests/babel-plugin-jsx-syntax/createImport.spec.ts
+++ tests/babel-plugin-jsx-syntax/createImport.spec.ts
@@ -7,4 +7,15 @@ describe('babel-plugin-jsx-syntax: createImport()', () => {
       'import{Fragment as _Fragment,svgNS as _svgNS,jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/_Fragment(/*#__PURE__*/_jsx("svg",{ns:_svgNS}));'
     );
   });
+
+  it('should add import before exist one', async () => {
+    const result = await t`
+      import _ from 'lodash';
+      const x = <div />;
+    `;
+
+    expect(result).toBe(
+      'import{jsx as _jsx}from"jsx-dom-runtime";import _ from"lodash";const x=/*#__PURE__*/_jsx("div",{});'
+    );
+  });
 });
