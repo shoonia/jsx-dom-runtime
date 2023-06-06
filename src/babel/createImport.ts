94398242cfbe3b673b78837319c7400391ab3f18
shoonia <alexanderz@wix.com>
2023-06-06 20:00:44 +0300

    Avoid mutation

--- src/babel/createImport.ts
+++ src/babel/createImport.ts
@@ -2,14 +2,16 @@ import type { NodePath } from '@babel/core';
 import t from '@babel/types';
 import { addNamed } from '@babel/helper-module-imports';
 
+import { $identifier } from './builders';
+
 export type TImportName = 'jsx' | 'Fragment' | 'svgNS' | 'mathmlNS' | 'xhtmlNS';
 
 export const createImport = (path: NodePath<t.Program>) => {
-  const cache = new Map<string, t.Identifier>();
+  const cache = new Map<string, string>();
 
   return (importName: TImportName): t.Identifier => {
     if (cache.has(importName)) {
-      return cache.get(importName);
+      return $identifier(cache.get(importName));
     }
 
     const newImport = addNamed(path, importName, 'jsx-dom-runtime', {
@@ -17,7 +19,7 @@ export const createImport = (path: NodePath<t.Program>) => {
       importPosition: 'after',
     });
 
-    cache.set(importName, newImport);
+    cache.set(importName, newImport.name);
 
     return newImport;
   };
