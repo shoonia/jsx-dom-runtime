c8d8c99516bfcfba75120d7ab785104bf2d839b8
shoonia <alexanderz@wix.com>
2023-06-06 19:54:31 +0300

    Clean code

--- src/babel/createImport.ts
+++ src/babel/createImport.ts
@@ -1,31 +1,23 @@
 import type { NodePath } from '@babel/core';
 import t from '@babel/types';
-import { addNamed, addNamespace } from '@babel/helper-module-imports';
+import { addNamed } from '@babel/helper-module-imports';
 
 export type TImportName = 'jsx' | 'Fragment' | 'svgNS' | 'mathmlNS' | 'xhtmlNS';
 
 export const createImport = (path: NodePath<t.Program>) => {
   const cache = new Map<string, t.Identifier>();
-  const isModule = path.node.sourceType === 'module';
-  const source = 'jsx-dom-runtime';
 
   return (importName: TImportName): t.Identifier => {
-    const key = isModule ? importName : source;
-
-    if (cache.has(key)) {
-      return cache.get(key);
+    if (cache.has(importName)) {
+      return cache.get(importName);
     }
 
-    const newImport = isModule
-      ? addNamed(path, importName, source, {
-        importedInterop: 'uncompiled',
-        importPosition: 'after',
-      })
-      : addNamespace(path, source, {
-        importedInterop: 'uncompiled',
-      });
+    const newImport = addNamed(path, importName, 'jsx-dom-runtime', {
+      importedInterop: 'uncompiled',
+      importPosition: 'after',
+    });
 
-    cache.set(key, newImport);
+    cache.set(importName, newImport);
 
     return newImport;
   };
