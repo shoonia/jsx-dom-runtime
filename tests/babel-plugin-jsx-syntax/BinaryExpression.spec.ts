925ddecfd2d7a4cde2f81ca8167a4f3e69302c50
shoonia <alexanderz@wix.com>
2023-07-30 16:03:59 +0300

    Fix test

--- tests/babel-plugin-jsx-syntax/BinaryExpression.spec.ts
+++ tests/babel-plugin-jsx-syntax/BinaryExpression.spec.ts
@@ -2,113 +2,112 @@ import { t } from '../utils';
 
 describe('babel-plugin-jsx-syntax: BinaryExpression', () => {
   it('should work with +', async () => {
-    const result = await t('<One /> + <Two />;');
+    const result = await t`<One /> + <Two />;`;
     expect(result).toBe('One({})+Two({});');
   });
 
   it('should work with -', async () => {
-    const result = await t('<One /> - <Two />;');
+    const result = await t`<One /> - <Two />;`;
     expect(result).toBe('One({})-Two({});');
   });
 
   it('should work with *', async () => {
-    const result = await t('<One /> * <Two />;');
+    const result = await t`<One /> * <Two />;`;
     expect(result).toBe('One({})*Two({});');
   });
 
   it('should work with **', async () => {
-    const result = await t('<One /> ** <Two />;');
+    const result = await t`<One /> ** <Two />;`;
     expect(result).toBe('One({})**Two({});');
   });
 
   it('should work with /', async () => {
-    const result = await t('<One /> / <Two />;');
+    const result = await t`<One /> / <Two />;`;
     expect(result).toBe('One({})/Two({});');
   });
 
   it('should work with >', async () => {
-    const result = await t('<One /> > <Two />;');
+    const result = await t`<One /> > <Two />;`;
     expect(result).toBe('One({})>Two({});');
   });
 
-  // FIXME: Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>? (1:8)
-  // it('should work with <', async () => {
-  //   const result = await t('<One /> < <Two />;');
-  //   expect(result).toBe('One({})<Two({});');
-  // });
+  it('should work with <', async () => {
+    const result = await t`(<One />) < <Two />;`;
+    expect(result).toBe('One({})<Two({});');
+  });
 
   it('should work with <<', async () => {
-    const result = await t('<One /> << <Two />;');
+    const result = await t`<One /> << <Two />;`;
     expect(result).toBe('One({})<<Two({});');
   });
 
   it('should work with >>', async () => {
-    const result = await t('<One /> >> <Two />;');
+    const result = await t`<One /> >> <Two />;`;
     expect(result).toBe('One({})>>Two({});');
   });
 
   it('should work with >>>', async () => {
-    const result = await t('<One /> >>> <Two />;');
+    const result = await t`<One /> >>> <Two />;`;
     expect(result).toBe('One({})>>>Two({});');
   });
 
   it('should work with >=', async () => {
-    const result = await t('<One /> >= <Two />;');
+    const result = await t`<One /> >= <Two />;`;
     expect(result).toBe('One({})>=Two({});');
   });
 
   it('should work with <=', async () => {
-    const result = await t('<One /> <= <Two />;');
+    const result = await t`<One /> <= <Two />;`;
     expect(result).toBe('One({})<=Two({});');
   });
 
   it('should work with %', async () => {
-    const result = await t('<One /> % <Two />;');
+    const result = await t`<One /> % <Two />;`;
     expect(result).toBe('One({})%Two({});');
   });
 
   it('should work with &', async () => {
-    const result = await t('<One /> & <Two />;');
+    const result = await t`<One /> & <Two />;`;
     expect(result).toBe('One({})&Two({});');
   });
 
   it('should work with ^', async () => {
-    const result = await t('<One /> ^ <Two />;');
+    const result = await t`<One /> ^ <Two />;`;
     expect(result).toBe('One({})^Two({});');
   });
 
   it('should work with |', async () => {
-    const result = await t('<One /> | <Two />;');
+    const result = await t`<One /> | <Two />;`;
     expect(result).toBe('One({})|Two({});');
   });
 
   it('should work with ==', async () => {
-    const result = await t('<One /> == <Two />;');
+    const result = await t`<One /> == <Two />;`;
     expect(result).toBe('One({})==Two({});');
   });
 
   it('should work with ===', async () => {
-    const result = await t('<One /> === <Two />;');
+    const result = await t`<One /> === <Two />;`;
     expect(result).toBe('One({})===Two({});');
   });
 
   it('should work with !=', async () => {
-    const result = await t('<One /> != <Two />;');
+    const result = await t`<One /> != <Two />;`;
     expect(result).toBe('One({})!=Two({});');
   });
 
   it('should work with !==', async () => {
-    const result = await t('<One /> !== <Two />;');
+    const result = await t`<One /> !== <Two />;`;
     expect(result).toBe('One({})!==Two({});');
   });
 
   it('should work with `instanceof`', async () => {
-    const result = await t('<One /> instanceof <Two />;');
+    const result = await t`<One /> instanceof <Two />;`;
     expect(result).toBe('One({})instanceof Two({});');
   });
 
   it('should work with `in`', async () => {
-    const result = await t('"key" in <App />;');
+    const result = await t`"key" in <App />;`;
     expect(result).toBe('"key"in App({});');
   });
 });
