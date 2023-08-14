7b6ea11e11a09f3b81b688756a297e0fec5d9f00
shoonia <alexanderz@wix.com>
2023-08-14 20:52:58 +0300

    Update dependencies

--- tests/babel-plugin-jsx-syntax/attr-boolean.spec.ts
+++ tests/babel-plugin-jsx-syntax/attr-boolean.spec.ts
@@ -4,37 +4,37 @@ const _jsx = 'import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/';
 
 describe('Babel transform HTML boolean attribute value `true` to empty string `""`', () => {
   it('should transform `readOnly` attribute', async () => {
-    const result = await t('<input readOnly />');
+    const result = await t`<input readOnly />`;
     expect(result).toBe(_jsx + '_jsx("input",{readonly:""});');
   });
 
   it('should transform `crossOrigin` attribute', async () => {
-    const result = await t('<img crossOrigin />');
+    const result = await t`<img crossOrigin />`;
     expect(result).toBe(_jsx + '_jsx("img",{crossorigin:""});');
   });
 
   it('should NOT transform `crossOrigin` attribute', async () => {
-    const result = await t('<img crossOrigin="anonymous" />');
+    const result = await t`<img crossOrigin="anonymous" />`;
     expect(result).toBe(_jsx + '_jsx("img",{crossorigin:"anonymous"});');
   });
 
   it('should transform `contentEditable` attribute', async () => {
-    const result = await t('<img contentEditable />');
+    const result = await t`<img contentEditable />`;
     expect(result).toBe(_jsx + '_jsx("img",{contenteditable:""});');
   });
 
   it('should NOT transform `contentEditable` attribute', async () => {
-    const result = await t('<img contentEditable="false" />');
+    const result = await t`<img contentEditable="false" />`;
     expect(result).toBe(_jsx + '_jsx("img",{contenteditable:"false"});');
   });
 
   it('should transform `capture` attribute', async () => {
-    const result = await t('<input capture />');
+    const result = await t`<input capture />`;
     expect(result).toBe(_jsx + '_jsx("input",{capture:""});');
   });
 
   it('should NOT transform `capture` attribute', async () => {
-    const result = await t('<input capture="user" />');
+    const result = await t`<input capture="user" />`;
     expect(result).toBe(_jsx + '_jsx("input",{capture:"user"});');
   });
 
@@ -46,5 +46,10 @@ describe('Babel transform HTML boolean attribute value `true` to empty string `"
   it('should transform `itemScope` attribute', async () => {
     const result = await t`<div itemScope />`;
     expect(result).toBe(_jsx + '_jsx("div",{itemscope:""});');
+  });
+
+  it('should transform `open` attribute', async () => {
+    const result = await t`<details open />`;
+    expect(result).toBe(_jsx + '_jsx("details",{open:""});');
   });
 });
