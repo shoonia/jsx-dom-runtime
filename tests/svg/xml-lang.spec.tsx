e2afa5a6ea7a5b86f7e22d664f056a5f0125087a
shoonia <alexanderz@wix.com>
2023-10-25 23:02:30 +0300

    Add tests

---
+++ tests/svg/xml-lang.spec.tsx
@@ -0,0 +1,18 @@
+import { t } from '../utils';
+
+const _jsx = 'import{svgNS as _svgNS,jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/';
+
+describe('SVG - `xml:lang` to `lang`', () => {
+  it('should render `xml:lang` attribute', () => {
+    expect(
+      <text xml:lang="en-US">This is some English text</text>
+    ).toHaveOuterHTML(
+      '<text lang="en-US">This is some English text</text>',
+    );
+  });
+
+  it('should transform attribute correctly', async () => {
+    const relust = await t`<text xml:lang="en-US">This is some English text</text>`;
+    expect(relust).toBe(_jsx +'_jsx("text",{lang:"en-US",children:"This is some English text",ns:_svgNS});');
+  });
+});
