2d97202c2d050869e54e827dbafb6e48ea470537
shoonia <alexanderz@wix.com>
2023-10-25 23:09:02 +0300

    Fix test

--- tests/svg/xml-lang.spec.tsx
+++ tests/svg/xml-lang.spec.tsx
@@ -1,6 +1,6 @@
 import { t } from '../utils';
 
-const _jsx = 'import{svgNS as _svgNS,jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/';
+const i = 'import{svgNS as _svgNS,jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/';
 
 describe('SVG - `xml:lang` to `lang`', () => {
   it('should render `xml:lang` attribute', () => {
@@ -13,6 +13,6 @@ describe('SVG - `xml:lang` to `lang`', () => {
 
   it('should transform attribute correctly', async () => {
     const relust = await t`<text xml:lang="en-US">This is some English text</text>`;
-    expect(relust).toBe(_jsx +'_jsx("text",{lang:"en-US",children:"This is some English text",ns:_svgNS});');
+    expect(relust).toBe(i +'_jsx("text",{lang:"en-US",children:"This is some English text",ns:_svgNS});');
   });
 });
