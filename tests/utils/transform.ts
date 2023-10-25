6bf3f96657f9e44490ffc8a119f019a04c7afeff
shoonia <alexanderz@wix.com>
2023-10-25 23:02:03 +0300

    Refactoring

--- tests/utils/transform.ts
+++ tests/utils/transform.ts
@@ -1,13 +1,14 @@
 import { transformAsync } from '@babel/core';
 
-import index from '../../babel-preset/index.cjs';
+import preset from '../../babel-preset/index.cjs';
 
 export const t = async (source: string | TemplateStringsArray): Promise<string> => {
   const { code } = await transformAsync(Array.isArray(source) ? source[0] : source, {
-    presets: [index],
+    presets: [preset],
     ast: false,
     minified: true,
     babelrc: false,
+    sourceMaps: false,
   });
 
   return code;
