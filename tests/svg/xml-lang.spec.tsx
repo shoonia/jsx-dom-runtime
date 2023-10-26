fc2913bb67b0e55078559b4ef92766546649f010
shoonia <alexanderz@wix.com>
2023-10-26 14:10:39 +0300

    Transform React styled `xmlLang` attribute to `lang`

--- tests/svg/xml-lang.spec.tsx
+++ tests/svg/xml-lang.spec.tsx
@@ -4,15 +4,27 @@ const i = 'import{svgNS as _svgNS,jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*
 
 describe('SVG - `xml:lang` to `lang`', () => {
   it('should render `xml:lang` attribute', () => {
-    expect(
-      <text xml:lang="en-US">This is some English text</text>
-    ).toHaveOuterHTML(
+    expect(<text xml:lang="en-US">This is some English text</text>).toHaveOuterHTML(
       '<text lang="en-US">This is some English text</text>',
     );
   });
 
   it('should transform attribute correctly', async () => {
     const relust = await t`<text xml:lang="en-US">This is some English text</text>`;
+    expect(relust).toBe(i +'_jsx("text",{lang:"en-US",children:"This is some English text",ns:_svgNS});');
+  });
+});
+
+describe('SVG - `xmlLang` to `lang`', () => {
+  it('should render `xmlLang` attribute', () => {
+    /* @ts-expect-error */
+    expect(<text xmlLang="en-US">This is some English text</text>).toHaveOuterHTML(
+      '<text lang="en-US">This is some English text</text>',
+    );
+  });
+
+  it('should transform attribute correctly', async () => {
+    const relust = await t`<text xmlLang="en-US">This is some English text</text>`;
     expect(relust).toBe(i +'_jsx("text",{lang:"en-US",children:"This is some English text",ns:_svgNS});');
   });
 });
