4741d71599c5e84489ce70f4b153f977186b6c78
shoonia <alexanderz@wix.com>
2023-11-26 07:31:37 +0200

    Add comments

--- index.d.ts
+++ index.d.ts
@@ -1811,8 +1811,14 @@ declare global {
     }
 
     interface HTMLTableDataCellElementAttributes extends HTMLAttributes<HTMLTableCellElement> {
+      /**
+       * A non-negative integer value that indicates for how many columns the cell extends. Its default value is `1`. Values higher than `1000` will be considered as incorrect and will be set to the default value (`1`)
+       */
       colSpan?: number | `${number}`
       headers?: string
+      /**
+       * A non-negative integer value that indicates for how many rows the cell extends. Its default value is `1`; if its value is set to `0`, it extends until the end of the table section (`<thead>`, `<tbody>`, `<tfoot>`, even if implicitly defined), that the cell belongs to. Values higher than `65534` are clipped down to `65534`
+       */
       rowSpan?: number | `${number}`
       /**
        * Do not use this attribute as it is obsolete in the latest standard. Alternatively, you can put the abbreviated description inside the cell and place the long content in the title attribute.
