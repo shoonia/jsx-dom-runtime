011ab32d0d6a5bb616edad21f00d95a7950dc210
shoonia <alexanderz@wix.com>
2023-12-23 19:53:12 +0200

    Inline collections

--- src/babel/collections.ts
+++ src/babel/collections.ts
@@ -1,11 +1,54 @@
-import svg from 'svg-tags';
-import html from 'html-tags';
+export const svgTags = new Set([
+  /* 'a', */ 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate', 'animateColor',
+  'animateMotion', 'animateTransform',
+  'circle', 'clipPath', 'color-profile', 'cursor',
+  'defs', 'desc',
+  'ellipse',
+  'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite',
+  'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight',
+  'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR', 'feGaussianBlur',
+  'feImage', 'feMerge', 'feMergeNode', 'feMorphology', 'feOffset', 'fePointLight',
+  'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence', 'filter', 'font',
+  'font-face', 'font-face-format', 'font-face-name', 'font-face-src', 'font-face-uri',
+  'foreignObject',
+  'g', 'glyph', 'glyphRef',
+  'hkern',
+  'image',
+  'line', 'linearGradient',
+  'marker', 'mask', 'metadata', 'missing-glyph', 'mpath',
+  'path', 'pattern', 'polygon', 'polyline',
+  'radialGradient', 'rect',
+  /* 'script', */ 'set', 'stop', /* 'style', */ 'svg', 'switch', 'symbol',
+  'text', 'textPath', /* 'title', */ 'tref', 'tspan',
+  'use',
+  'view', 'vkern'
+]);
 
-// The tags, that exist in HTML and SVG too
-const crossTags = new Set(['a', 'script', 'style', 'title']);
-
-export const svgTags = new Set(svg.filter((i) => !crossTags.has(i)));
-export const htmlTags = new Set<string>(html);
+export const htmlTags = new Set([
+  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio',
+  'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button',
+  'canvas', 'caption', 'cite', 'code', 'col', 'colgroup',
+  'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt',
+  'em', 'embed',
+  'fieldset', 'figcaption', 'figure', 'footer', 'form',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html',
+  'i', 'iframe', 'img', 'input', 'ins',
+  'kbd',
+  'label', 'legend', 'li', 'link',
+  'main', 'map', 'mark', 'math', 'menu', 'menuitem', 'meta', 'meter',
+  'nav', 'noscript',
+  'object', 'ol', 'optgroup', 'option', 'output',
+  'p', 'param', 'picture', 'pre', 'progress',
+  'q',
+  'rb', 'rp', 'rt', 'rtc', 'ruby',
+  's', 'samp', 'script', 'search', 'section', 'select', 'slot', 'small', 'source',
+  'span', 'strong', 'style', 'sub', 'summary', 'sup',
+  'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time',
+  'title', 'tr', 'track',
+  'u', 'ul',
+  'var', 'video',
+  'wbr',
+]);
 
 export const mathmlTags = new Set([
   'annotation', 'annotation-xml', 'maction', 'math', 'merror', 'mfrac', 'mi',
