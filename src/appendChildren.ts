6d7d476dd3a71f0cae2f5810118f3d80bf1dadfd
shoonia <alexanderz@wix.com>
2024-03-09 19:41:28 +0200

    Code Golf

--- src/appendChildren.ts
+++ src/appendChildren.ts
@@ -2,7 +2,7 @@ export const appendChildren = (children, node) => {
   if (Array.isArray(children)) {
     // Just shorter that the .forEach
     children.some((child) => appendChildren(child, node));
-  } else if (children != null && children !== false) {
+  } else if (children !== false && children != null) {
     node.append(children);
   }
 };
