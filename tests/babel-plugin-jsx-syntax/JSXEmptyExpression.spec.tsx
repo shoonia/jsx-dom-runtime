69da230eea1ab18b5d68cfd39869e3b388d57256
shoonia <alexanderz@wix.com>
2024-05-05 19:08:13 +0300

    Add test

--- tests/babel-plugin-jsx-syntax/JSXEmptyExpression.spec.tsx
+++ tests/babel-plugin-jsx-syntax/JSXEmptyExpression.spec.tsx
@@ -8,10 +8,17 @@ describe('babel: JSXEmptyExpression', () => {
   });
 
   it('should be ignored #2', async () => {
-    await expect('<div>Hello{}, World!</div>').toBeTransform(i + '_jsx("div",{children:["Hello",", World!"]});');
+    await expect('<div>Hello{}, World!</div>')
+      .toBeTransform(i + '_jsx("div",{children:["Hello",", World!"]});');
   });
 
-  it('should be syntax error', async () => {
+  it('should be syntax error #1', async () => {
+    expect.hasAssertions();
     await expect(t`<div id={}></div>`).rejects.toBeInstanceOf(SyntaxError);
+  });
+
+  it('should be syntax error #2', async () => {
+    expect.hasAssertions();
+    await expect(t`<div {}></div>`).rejects.toBeInstanceOf(SyntaxError);
   });
 });
