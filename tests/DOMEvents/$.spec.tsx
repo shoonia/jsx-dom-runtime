28f0aa7686c4696a8b4621c445bb8dec099905aa
shoonia <alexanderz@wix.com>
2024-05-06 17:02:26 +0300

    Update types

--- tests/DOMEvents/$.spec.tsx
+++ tests/DOMEvents/$.spec.tsx
@@ -5,8 +5,8 @@ import { jsx } from '../..';
 
 describe('$', () => {
   it('should add a few handlers', () => {
-    const spyClick = jest.fn();
-    const spyChange = jest.fn();
+    const spyClick: JSX.EventListener = jest.fn();
+    const spyChange: JSX.EventListener = jest.fn();
     const input = <input on:click={spyClick} on:change={spyChange} />;
 
     fireEvent.click(input);
@@ -17,32 +17,78 @@ describe('$', () => {
     expect(input).toHaveValue('xyz');
   });
 
-  it('should add event listener in $ props', () => {
-    const click = jest.fn();
-    const myEvent = jest.fn();
+  it('should add event listener in $ props #1', () => {
+    const click: JSX.EventListener = jest.fn();
+    const myEvent: JSX.EventListener = jest.fn();
 
-    const div = <div $={{click, 'my-event': myEvent}} /> as HTMLElement;
-
-    div.click();
-    div.dispatchEvent(new CustomEvent('my-event'));
+    <div
+      $={{
+        click,
+        'my-event': myEvent,
+      }}
+      ref={(div) => {
+        div.click();
+        div.dispatchEvent(new CustomEvent('my-event'));
+      }}
+    />;
 
     expect(click).toHaveBeenCalledTimes(1);
     expect(myEvent).toHaveBeenCalledTimes(1);
   });
 
-  it('should add event listener in $ props', () => {
-    const click = jest.fn();
-    const myEvent = jest.fn();
+  it('should add event listener in $ props #2', () => {
+    const click: JSX.EventListener = jest.fn();
+    const myEvent: JSX.EventListener = jest.fn();
 
-    const div = jsx('div', {
+    jsx('div', {
       $: {
         click,
         'my-event': myEvent,
       },
+      ref(div) {
+        div.click();
+        div.dispatchEvent(new CustomEvent('my-event'));
+      }
     });
 
-    div.click();
-    div.dispatchEvent(new CustomEvent('my-event'));
+    expect(click).toHaveBeenCalledTimes(1);
+    expect(myEvent).toHaveBeenCalledTimes(1);
+  });
+
+  it('should add event object listener in $ props (#3)', () => {
+    const click: JSX.EventListener = jest.fn();
+    const myEvent: JSX.EventListener = jest.fn();
+
+    <div
+      $={{
+        click: { handleEvent: click },
+        'my-event': { handleEvent: myEvent }
+      }
+      }
+      ref={(div) => {
+        div.click();
+        div.dispatchEvent(new CustomEvent('my-event'));
+      }}
+    />;
+
+    expect(click).toHaveBeenCalledTimes(1);
+    expect(myEvent).toHaveBeenCalledTimes(1);
+  });
+
+  it('should add object event listener in $ props (#4)', () => {
+    const click: JSX.EventListener = jest.fn();
+    const myEvent: JSX.EventListener = jest.fn();
+
+    jsx('div', {
+      $: {
+        click: { handleEvent: click },
+        'my-event': { handleEvent: myEvent },
+      },
+      ref(div) {
+        div.click();
+        div.dispatchEvent(new CustomEvent('my-event'));
+      }
+    });
 
     expect(click).toHaveBeenCalledTimes(1);
     expect(myEvent).toHaveBeenCalledTimes(1);
