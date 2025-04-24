372a8de7de08228592a68cdba03318290ce5110d
shoonia <alexanderz@wix.com>
2025-04-24 17:43:20 +0300

    Update tests

--- tests/attr-directive.spec.tsx
+++ tests/attr-directive.spec.tsx
@@ -1,27 +1,27 @@
 describe('attr:* directive', () => {
   it('should transform `attr:` directive with value', async () => {
-    await expect('<input attr:foo="bar" />').toBeTransform(
-      'import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/_jsx("input",{foo:"bar"});'
+    await expect('<div attr:foo="bar" />').toBeTransform(
+      'import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/_jsx("div",{foo:"bar"});'
     );
   });
 
   it('should correct transform `attr:` directive with value', async () => {
-    await expect('<input attr:foo-foo={1} />').toBeTransform(
-      'import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/_jsx("input",{"foo-foo":1});'
+    await expect('<div attr:foo-foo={1} />').toBeTransform(
+      'import{jsx as _jsx}from"jsx-dom-runtime";/*#__PURE__*/_jsx("div",{"foo-foo":1});'
     );
   });
 
   it('should render using `attr:` directive to set attributes', () => {
-    const input = <input attr:foo="bar" />;
+    const div = <div attr:foo="bar" />;
 
-    expect(input).toHaveAttribute('foo', 'bar');
-    expect(input).not.toHaveProperty('foo');
+    expect(div).toHaveAttribute('foo', 'bar');
+    expect(div).not.toHaveProperty('foo');
   });
 
-  it.each([1, 1n, 0, 'hello'])('should correct render using `attr:` directive to set attributes', (value) => {
-    const input = <input attr:foo-foo={value} />;
+  it.each([1, 1n, 0, Infinity, NaN])('should correct render using `attr:` directive to set attributes', (value) => {
+    const div = <div attr:foo-foo={value} />;
 
-    expect(input).toHaveAttribute('foo-foo', value.toString());
-    expect(input).not.toHaveProperty('foo-foo');
+    expect(div).toHaveAttribute('foo-foo', value.toString());
+    expect(div).not.toHaveProperty('foo-foo');
   });
 });
