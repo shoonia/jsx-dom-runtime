05a367525e103e3f7ceccdf867ca7d80b74ba817
shoonia <alexanderz@wix.com>
2025-08-28 18:38:31 +0300

    test: add support for additional props in HTMLOListElement tests

--- tests/HTMLOListElement.spec.tsx
+++ tests/HTMLOListElement.spec.tsx
@@ -22,4 +22,22 @@ describe('HTMLOListElement', () => {
   it('should have `reversed` attribute with string value', () => {
     expect(<ol reversed="" />).toHaveProperty('reversed', true);
   });
+
+  it('should support prop:compact', () => {
+    expect(<ol prop:compact />).toHaveProperty('compact', true);
+  });
+
+  it('should support prop:reversed true/false', () => {
+    expect(<ol prop:reversed />).toHaveProperty('reversed', true);
+    expect(<ol prop:reversed={false} />).toHaveProperty('reversed', false);
+  });
+
+  it('should support prop:start', () => {
+    expect(<ol prop:start={5} />).toHaveProperty('start', 5);
+  });
+
+  it('should support prop:type', () => {
+    expect(<ol prop:type="a" />).toHaveProperty('type', 'a');
+    expect(<ol prop:type="1" />).toHaveProperty('type', '1');
+  });
 });
