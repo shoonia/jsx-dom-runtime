c644c3faf594646fa8017c2fd03b8ba70a1a6512
Alexander Zaytsev <zaytsev126@gmail.com>
2025-12-05 18:02:46 +0200

    Add cross tags (#376)
    * feat: add support for crossTags in isSvgNode function
    
    * feat: update publish workflow to trigger on branches matching '*--publish'
    
    * style: format publish.yml for consistency and readability

--- src/eslint/utils.ts
+++ src/eslint/utils.ts
@@ -3,6 +3,8 @@ import type { TSESTree } from '@typescript-eslint/utils';
 import { htmlTags, svgTags, mathmlTags, voidHTMLTags } from '../collections';
 
 const voidTags = new Set(voidHTMLTags);
+// Tags that are allowed in both HTML and SVG contexts
+const crossTags = new Set(['a', 'script', 'style', 'title']);
 
 const isStandardTag = (tag: string) => htmlTags.has(tag) || svgTags.has(tag) || mathmlTags.has(tag);
 
@@ -15,7 +17,7 @@ export const isStandardNode = (node: TSESTree.JSXOpeningElement): boolean =>
 
 export const isSvgNode = (node: TSESTree.JSXOpeningElement): boolean =>
   isJSXIdentifier(node.name) &&
-  (svgTags.has(node.name.name) || node.name.name === 'a');
+  (svgTags.has(node.name.name) || crossTags.has(node.name.name));
 
 export const isVoidElement = (node: TSESTree.JSXOpeningElement): boolean =>
   isJSXIdentifier(node.name) &&
