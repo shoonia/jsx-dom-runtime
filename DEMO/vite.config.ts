52c72dff1790ee5cc5d00522e45d4df2574db307
shoonia <zaytsev126@gmail.com>
2026-01-27 20:54:08 +0200

    chore: update vite to version 7.3.1 and refactor configuration
    - Updated vite dependency from ^5.4.0 to ^7.3.1 in package.json.
    - Refactored vite.config.ts:
      - Changed mini-css-class-name import to generateScopedName.
      - Adjusted build options for production and development modes.
      - Enabled sourcemaps in development mode.
      - Updated CSS modules configuration for scoped names based on environment.
      - Enhanced esbuild options for better performance and debugging.

--- DEMO/vite.config.ts
+++ DEMO/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import babel from '@rollup/plugin-babel';
-import miniCssClassName from 'mini-css-class-name/postcss-modules';
+import generateScopedName from 'mini-css-class-name/postcss-modules';
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
-  const generateScopedName = isProd
-    ? miniCssClassName()
-    : '[name]__[local]___[hash:base64:5]';
+  const isDev = mode === 'development';
 
   return {
     root: 'src',
@@ -14,55 +12,42 @@ export default defineConfig(({ mode }) => {
     server: { open: true },
     build: {
       outDir: '../dist',
+      assetsDir: '.',
       emptyOutDir: true,
       target: 'esnext',
       minify: isProd ? 'terser' : false,
-      sourcemap: false,
-      polyfillModulePreload: false,
+      sourcemap: isDev,
+      modulePreload: false,
       reportCompressedSize: true,
-      terserOptions: {
-        ecma: 2020,
-        module: true,
-        toplevel: true,
-        compress: {
-          ecma: 2020,
-          module: true,
-          comparisons: false,
-          inline: 2,
-          drop_console: false,
-          passes: 3,
-          toplevel: true,
-          pure_getters: true,
-          unsafe: true,
-          unsafe_arrows: true,
-          unsafe_undefined: true,
-          unsafe_math: true,
-          unsafe_symbols: true,
+      rollupOptions: {
+        output: {
+          compact: true,
         },
+        treeshake: true,
       },
-    },
-    rollupOptions: {
-      output: {
-        compact: true,
-      },
-      treeshake: true,
     },
     css: {
       modules: {
-        generateScopedName,
+        generateScopedName: isProd
+          ? generateScopedName()
+          : '[name]__[local]___[hash:base64:5]',
       },
     },
     esbuild: {
       jsx: 'preserve',
       legalComments: 'none',
       drop: ['console', 'debugger'],
-      keepNames: false,
+      keepNames: isDev,
+      treeShaking: true,
+      minifyIdentifiers: isProd,
+      minifySyntax: isProd,
+      minifyWhitespace: isProd,
     },
     plugins: [
       babel({
         extensions: ['.ts', '.tsx'],
         babelHelpers: 'bundled',
-        comments: false,
+        comments: isDev,
         presets: ['jsx-dom-runtime/babel-preset'],
       }),
     ],
