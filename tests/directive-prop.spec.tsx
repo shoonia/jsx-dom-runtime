1670ae8d1f416671452fd1152b03772968c8f8b0
shoonia <alexanderz@wix.com>
2026-07-26 15:04:19 +0300

    refactor: consolidate prop tests into directive prop tests and remove redundant file

--- tests/directive-prop.spec.tsx
+++ tests/directive-prop.spec.tsx
@@ -1,6 +1,23 @@
 import { jsxImport } from './utils';
 
 describe('directive prop:*', () => {
+  it('should support `id` property', () => {
+    expect(<p prop:id="a" />).toHaveAttribute('id', 'a');
+    expect(<p prop:id="a" />).toHaveProperty('id', 'a');
+  });
+
+  it('should support `textContent` property', () => {
+    expect(<span prop:textContent="hello" />).toHaveTextContent('hello');
+  });
+
+  it('should support `innerHTML` property', () => {
+    expect(<div prop:innerHTML="<p>text</p>" />).toHaveInnerHTML('<p>text</p>');
+  });
+
+  it('should support `classList` property', () => {
+    expect(<div prop:classList="a-class" />).toHaveClass('a-class');
+  });
+
   it('should transform `prop:*` directive with value', async () => {
     await expect('<div prop:foo="bar" />').toBeTransform(
       jsxImport`_jsx("div",{ref:e=>e.foo="bar"});`
