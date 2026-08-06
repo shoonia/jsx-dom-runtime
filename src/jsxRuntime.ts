374af1daab4588c25d8f0b520e14aa31911f5301
shoonia <alexanderz@wix.com>
2026-08-06 16:26:14 +0300

    refactor: move utility functions to separate module

--- src/jsxRuntime.ts
+++ src/jsxRuntime.ts
@@ -2,32 +2,6 @@
 const svgNs = 'http://www.w3.org/2000/svg';
 const mathmlNs = 'http://www.w3.org/1998/Math/MathML';
 
-const setStyle = (node, value) => {
-  if (typeof value == 'string') {
-    node.style.cssText = value;
-  } else {
-    for (let key in value) {
-      if (key.startsWith('-')) {
-        node.style.setProperty(key, value[key]);
-      } else {
-        node.style[key] = value[key];
-      }
-    }
-  }
-};
-
-const setDataset = (node, value) => {
-  for (let key in value) {
-    if (value[key] != null) {
-      node.dataset[key] = value[key];
-    }
-  }
-};
-
-const setAttributes = (node, value) =>
-  (Array.isArray(value) ? value : [value]).forEach(
-    (i) => node.setAttributeNode(i));
-
 const render = (content, node) =>
   content !== false && content != null && (
     Array.isArray(content)
@@ -80,9 +54,6 @@ const jsx = (tag, props, children?: any) => {
 export {
   jsx,
   render,
-  setStyle,
-  setDataset,
-  setAttributes,
   svgNs,
   mathmlNs,
 };
