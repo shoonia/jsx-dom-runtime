6973334e2b3f6698b5de810aa9c6a6c7e755d1da
shoonia <alexanderz@wix.com>
2026-08-14 00:18:06 +0300

    fix: add JSX element with prop:className attribute for signal

--- tests/signal/type.tsx
+++ tests/signal/type.tsx
@@ -3,6 +3,7 @@ import { signal } from 'jsx-dom-runtime';
 const className = signal('my-class');
 className.set('new-class');
 <div class={className} />;
+<div prop:className={className} />;
 
 const maxValue = signal(100);
 maxValue.set(200);
